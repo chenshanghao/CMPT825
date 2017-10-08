13a8aa76d62ec9661cc7507bcd378366f287394f
yingxiuc <yingxiuc@sfu.ca>
2017-10-08 14:50:13 -0700

    yingxiuc20171008 1. Based on unigram modified program to corporate bigram 2. Add HeadAdd1Smooth to calculate prob. of leading word of each sentence 3. Add UnigramAdd1Smooth to calculate prob of unigram 4. Add BigramProb to calculate prob. of bigram. 5. Add Jelinek-Mercer smoothing function to calculate P(wi|wi-1) 6. Format code based on pylint. yingxiuc20170929 Initial version of hw-1. 1. Iterative DP on unigram 2. Add 1 smoothing 3. Accept at most 4-char-long unknown, but takes 1.5h to finish, needs to be refined.

--- HW1-segmenter/answer/README.yingxiuc.md
+++ HW1-segmenter/answer/README.yingxiuc.md
@@ -1,10 +1,12 @@
+yingxiuc20171008
+1. Based on unigram modified program to corporate bigram
+2. Add HeadAdd1Smooth to calculate prob. of leading word of each sentence
+3. Add UnigramAdd1Smooth to calculate prob of unigram
+4. Add BigramProb to calculate prob. of bigram.
+5. Add Jelinek-Mercer smoothing function to calculate P(wi|wi-1)
+6. Format code based on pylint.
 yingxiuc20170929
 Initial version of hw-1.
 1. Iterative DP on unigram
 2. Add 1 smoothing
 3. Accept at most 4-char-long unknown, but takes 1.5h to finish, needs to be refined.
-Your documentation
-------------------
-
-Make it so.
-
--- HW1-segmenter/answer/segment.py
+++ HW1-segmenter/answer/segment.py
@@ -1,79 +1,159 @@
-import sys, codecs, optparse, os
-import collections, heapq, math
+'''
+segmenter for 2 word
+'''
+import codecs
+import collections
+import heapq
+import math
+import optparse
+import os
+import sys
 
-optparser = optparse.OptionParser()
-optparser.add_option("-c", "--unigramcounts", dest='counts1w', default='D:\\Courses\\825\\homework\\nlp-class-hw\\segmenter\\data\\count_1w.txt', help="unigram counts")
-optparser.add_option("-b", "--bigramcounts", dest='counts2w', default='D:\\Courses\\825\\homework\\nlp-class-hw\\segmenter\\data\\count_2w.txt', help="bigram counts")
-optparser.add_option("-i", "--inputfile", dest="input", default='D:\\Courses\\825\\homework\\nlp-class-hw\\segmenter\\data\\input', help="input file to segment")
-(opts, _) = optparser.parse_args()
+OPTPARSER = optparse.OptionParser()
+OPTPARSER.add_option("-c", "--unigramcounts",
+                     dest='counts1w',
+                     default=os.path.join('data', 'count_1w.txt'),
+                     help="unigram counts")
+OPTPARSER.add_option("-b", "--bigramcounts",
+                     dest='counts2w',
+                     default=os.path.join('data', 'count_2w.txt'),
+                     help="bigram counts")
+OPTPARSER.add_option("-i", "--inputfile",
+                     dest="input",
+                     default=os.path.join('data', 'input'),
+                     help="input file to segment")
+(OPTS, _) = OPTPARSER.parse_args()
 
-input_len=0
-with open(opts.input) as f:
+INPUT_LEN = 0
+with open(OPTS.input) as f:
     for l in f:
-        input_len += len(unicode(l.strip(), 'utf-8'))
-#print input_len
+        INPUT_LEN += len(unicode(l.strip(), 'utf-8'))
+
+INPUT_LINE_CNT = 0
+with open(OPTS.input) as f:
+    for l in f:
+        INPUT_LINE_CNT += 1
+
+#print INPUT_LEN
 class Pdist(dict):
     "A probability distribution estimated from counts in datafile."
 
     def __init__(self, filename, sep='\t', N=None, missingfn=None):
         self.maxlen = 0
-        for line in file(filename):
-            (key, freq) = line.split(sep)
+        for per_line in file(filename):
+            (key, freq) = per_line.split(sep)
             try:
                 utf8key = unicode(key, 'utf-8')
             except:
                 raise ValueError("Unexpected error %s" % (sys.exc_info()[0]))
-            self[utf8key] = self.get(utf8key, 0) + int(freq)
-            self.maxlen = max(len(utf8key), self.maxlen)
-            
-        self.N = float(N or sum(self.itervalues()))+input_len
-        self.missingfn = missingfn or (lambda k, N: len(k)*math.log10(float(1.)/N) )
+            self[utf8key] = self.get(utf8key, 0)+int(freq)
+            if u' ' in utf8key:
+                self.maxlen = max(len(utf8key.split()[0]), len(utf8key.split()[1]), self.maxlen)
+            else:
+                self.maxlen = max(len(utf8key), self.maxlen)
+        self.total = float(N or sum(self.itervalues()))
+        self.missingfn = missingfn or (lambda k, total: len(k))
 
     def __call__(self, key):
         if key in self:
-            return math.log10(float(1+self[key])/float(self.N))#78.2->78.3
-        elif len(key) <= 4:#1:80;2:83;4:83.88
-            return self.missingfn(key, self.N)
+            return self[key]
         else:
             return None
 
 # the default segmenter does not use any probabilities, but you could ...
-Pw  = Pdist(opts.counts1w)
+PW1 = Pdist(OPTS.counts1w)
+PW2 = Pdist(OPTS.counts2w)
 
 #Entry for each line
 Entry = collections.namedtuple('Entry', ['start_pos', 'log_prob', 'word', 'prev_entry'])
 #min heap queue storing start_pos
 hp = []
-
 old = sys.stdout
 sys.stdout = codecs.lookup('utf-8')[-1](sys.stdout)
-# ignoring the dictionary provided in opts.counts
-with open(opts.input) as f:
+# ignoring the dictionary provided in OPTS.counts
+def HeadAdd1Smooth(w):
+    if PW2(u'<S> '+w) is None:
+        return (1.0/(INPUT_LINE_CNT+PW2.total))**len(w)
+    else:
+        return (1.0+PW2(u'<S> '+w))/(INPUT_LINE_CNT+PW2.total)
+
+def UnigramAdd1Smooth(w):
+    '''P(w)=1+C(w)/V+C(w)
+    '''
+    if PW1(w) is None:
+        w1count = 0
+    else: w1count = PW1(w)*1.0
+    return ((1+w1count)/(INPUT_LEN+PW1.total), w1count)
+
+def BigramProb(w2):
+    '''prev next
+    P(next|prev)=C(prev,next)/C(prev)
+    '''
+    prev_w = w2.split()[0]
+    if PW2(w2) is None:
+        return 0
+    #if w2 exist in pw2, prev and next will definitely appear in pw1
+    w2count = PW2(w2)*1.0
+    w1count = PW1(prev_w)*1.0
+    return w2count/w1count
+
+def JMSmooth(w2):
+    '''P(w2|w1)=lambda*P(w2|w1)+(1-lambda)*P(w2)
+    '''
+    jm_x = 0.9
+    #jm_prev = w2.split()[0]
+    jm_next = w2.split()[1]
+    unigram_res = UnigramAdd1Smooth(jm_next)
+    if unigram_res[1] == 0:
+        prob = jm_x*BigramProb(w2)+(1.0-jm_x)*(unigram_res[0]**len(jm_next))
+    else:
+        prob = jm_x*BigramProb(w2)+(1.0-jm_x)*(unigram_res[0])
+    return math.log10(prob)
+
+def w2len(w):
+    w = w.split()
+    if w[0] == u'<S>':
+        return len(w[1])
+    else:
+        return len(w[0])+len(w[1])
+
+with open(OPTS.input) as f:
     for line in f:
         utf8line = unicode(line.strip(), 'utf-8')
         #print utf8line
         line_len = len(utf8line)
+        max_len = min(line_len, PW2.maxlen)
         chart = [None]*(line_len)
-        for i in xrange(min(line_len, Pw.maxlen)):
-            possible_word = utf8line[:i+1]
-            if Pw(possible_word) != None:
-                heapq.heappush(hp, Entry(start_pos=0, word=possible_word, log_prob=Pw(possible_word), prev_entry=None))
+
+        for i in xrange(1, 4+max_len):#'<S> ' len is 4
+            head = utf8line[:i]
+            if len(head) <= 4:
+                heapq.heappush(hp, Entry(start_pos=0,
+                                         word=u'<S> '+head,
+                                         log_prob=math.log10(HeadAdd1Smooth(head)),
+                                         prev_entry=None))
+
         while hp:
             entry = heapq.heappop(hp)
-            endindex = entry.start_pos+len(entry.word)-1
+            endindex = entry.start_pos+w2len(entry.word)-1
             if chart[endindex] != None:
-                    if entry.log_prob > chart[endindex].log_prob:
-                        chart[endindex] = entry
-                    else:
-                        continue
+                if entry.log_prob > chart[endindex].log_prob:
+                    chart[endindex] = entry
+                else:
+                    continue
             else:
                 chart[endindex] = entry
+
+            entry_word2 = entry.word.split()[1]
             for j in xrange(endindex+1, line_len):
-                possible_newword = utf8line[endindex+1:j+1]
-                if len(possible_newword) > Pw.maxlen:
+                next_word = entry_word2+u' '+utf8line[endindex+1:j+1]
+                if j-endindex > PW2.maxlen:
                     break
-                if Pw(possible_newword) != None:
-                    newentry = Entry(start_pos=endindex+1, word=possible_newword, log_prob=entry.log_prob+Pw(possible_newword), prev_entry=entry)
+                if j-endindex <= 5:
+                    newentry = Entry(start_pos=endindex-len(entry_word2)+1,
+                                     word=next_word,
+                                     log_prob=entry.log_prob+JMSmooth(next_word),
+                                     prev_entry=entry)
                     if newentry not in hp:
                         heapq.heappush(hp, newentry)
 
@@ -82,9 +162,9 @@ with open(opts.input) as f:
         tmp = finalentry
         res = []
         while tmp.prev_entry != None:
-            res=[tmp.word]+res
+            res = [tmp.word.split()[1]]+res
             tmp = tmp.prev_entry
-        res=[tmp.word]+res
-        print " ".join(res)
+        res = [tmp.word.split()[1]]+res
+        print ' '.join(res)
 
 sys.stdout = old
