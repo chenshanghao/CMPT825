8d5f5ea9d5ca580b380cab6eac3be97570407733
yingxiuc <yingxiuc@sfu.ca>
2017-10-27 09:49:54 -0700

    yingxiuc20171027 initialize ver.

--- HW3-aligner/answer/align.py
+++ HW3-aligner/answer/align.py
@@ -7,7 +7,6 @@ import logging
 import optparse
 import os
 import sys
-from collections import defaultdict
 
 OPTPARSER = optparse.OptionParser()
 OPTPARSER.add_option("-d", "--datadir", dest="datadir", default="data", help="data directory (default=data)")
@@ -16,7 +15,7 @@ OPTPARSER.add_option("-e", "--english", dest="english", default="en", help="suff
 OPTPARSER.add_option("-f", "--french", dest="french", default="fr", help="suffix of French (source language) filename (default=fr)")
 OPTPARSER.add_option("-l", "--logfile", dest="logfile", default=None, help="filename for logging output")
 OPTPARSER.add_option("-t", "--threshold", dest="threshold", default=0.5, type="float", help="threshold for alignment (default=0.5)")
-OPTPARSER.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to use for training and alignment")
+OPTPARSER.add_option("-n", "--num_sentences", dest="num_sents", default=1000, type="int", help="Number of sentences to use for training and alignment")
 (OPTS, _) = OPTPARSER.parse_args()
 f_data = "%s.%s" % (os.path.join(OPTS.datadir, OPTS.fileprefix), OPTS.french)
 e_data = "%s.%s" % (os.path.join(OPTS.datadir, OPTS.fileprefix), OPTS.english)
@@ -24,31 +23,53 @@ e_data = "%s.%s" % (os.path.join(OPTS.datadir, OPTS.fileprefix), OPTS.english)
 if OPTS.logfile:
     logging.basicConfig(filename=OPTS.logfile, filemode='w', level=logging.INFO)
 
-sys.stderr.write("Training with Dice's coefficient...")
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:OPTS.num_sents]]
-f_count = defaultdict(int)
-e_count = defaultdict(int)
-fe_count = defaultdict(int)
-for (n, (f, e)) in enumerate(bitext):
-    for f_i in set(f):
-        f_count[f_i] += 1
-    for e_j in set(e):
-        fe_count[(f_i, e_j)] += 1
-    for e_j in set(e):
-        e_count[e_j] += 1
-    if n % 500 == 0:
-        sys.stderr.write(".")
+dice = {}
 
-dice = defaultdict(int)
-for (k, (f_i, e_j)) in enumerate(fe_count.keys()):
-    dice[(f_i, e_j)] = 2.0 * fe_count[(f_i, e_j)] / (f_count[f_i] + e_count[e_j])
-    if k % 5000 == 0:
-        sys.stderr.write(".")
-sys.stderr.write("\n")
+def init():
+    '''uniformly initilize fe_count'''
+    f_word_len = len(set([w for s in open(f_data) for w in s.strip().split()]))
+    return 1.0/f_word_len
 
-for (f, e) in bitext:
-    for (i, f_i) in enumerate(f):
-        for (j, e_j) in enumerate(e):
-            if dice[(f_i, e_j)] >= OPTS.threshold:
-                sys.stdout.write("%i-%i " % (i, j))
-    sys.stdout.write("\n")
+
+def train():
+    '''train body, dice is t_k'''
+    dice_default = init()
+    sys.stderr.write("Training with Dice's coefficient...\n")
+    for k in range(3):
+        sys.stderr.write("Iteration {0} ".format(k))
+        f_count = {}
+        e_count = {}
+        fe_count = {}
+        for (f, e) in bitext:
+            for f_i in set(f):
+                Z = 0.0
+                for e_j in set(e):
+                    Z += dice.get((f_i, e_j), dice_default)
+                for e_j in set(e):
+                    c = dice.get((f_i, e_j), dice_default)/Z
+                    fe_count[(f_i, e_j)] = fe_count.get((f_i, e_j), 0.0)+c
+                    e_count[e_j] = e_count.get(e_j, 0.0)+c
+        for (it, (f_i, e_j)) in enumerate(fe_count.keys()):
+            dice[(f_i, e_j)] = fe_count[(f_i, e_j)] / e_count[e_j]
+            if it % 500000 == 0:
+                sys.stderr.write(".")
+        sys.stderr.write("\n")
+
+def decode():
+    for (f, e) in bitext:
+        for i, f_i in enumerate(f):
+            bestp, bestj = 0.0, 0.0
+            for j, e_j in enumerate(e):
+                if dice[(f_i, e_j)] > bestp:
+                    bestp = dice[(f_i, e_j)]
+                    bestj = j
+            sys.stdout.write("%i-%i " % (i, bestj))
+
+def main():
+    '''main'''
+    train()
+    decode()
+
+if __name__ == "__main__":
+    main()
