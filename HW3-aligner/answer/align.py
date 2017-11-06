1be4d14828c4a435ed495317bf1c2bf153782884
yingxiuc <yingxiuc@sfu.ca>
2017-11-05 21:02:06 -0800

    yingxiuc20171105 temporary code.

--- HW3-aligner/answer/align.py
+++ HW3-aligner/answer/align.py
@@ -25,19 +25,52 @@ if OPTS.logfile:
 
 bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_data), open(e_data))[:OPTS.num_sents]]
 dice = {}
+dice_ef = {}
+dice_fe = {}
 
-def init():
-    '''uniformly initilize fe_count'''
-    f_word_len = len(set([w for s in open(f_data) for w in s.strip().split()]))
-    return 1.0/f_word_len
+def train_ef():
+    '''train e|f, dice is t_k'''
+    def init():
+        '''uniformly initilize ef_count'''
+        e_word_len = len(set([w for s in open(e_data) for w in s.strip().split()]))
+        return 1.0/e_word_len
 
-
-def train():
-    '''train body, dice is t_k'''
     dice_default = init()
-    sys.stderr.write("Training with expected maximization algorithm...\n")
     for k in range(5):
-        sys.stderr.write("Iteration {0} ".format(k))
+        sys.stderr.write("train_ef: Iteration {0} ".format(k))
+        f_count = {}
+        fe_count = {}
+
+        for (f, e) in bitext:
+            for e_j in e:
+                Z = 0.0
+                for f_i in f:
+                    if (f_i, e_j) not in dice_ef: dice_ef[(f_i, e_j)] = dice_default
+                    Z += dice_ef[(f_i, e_j)]
+                for f_i in f:
+                    c = dice_ef[(f_i, e_j)]/Z
+                    fe_count[(f_i, e_j)] = fe_count.get((f_i, e_j), 0.0)+c
+                    f_count[f_i] = f_count.get(f_i, 0.0)+c
+        loss = 0.0
+        for (it, (f_i, e_j)) in enumerate(fe_count.keys()):
+            loss = max(abs(dice_ef[(f_i, e_j)]-fe_count[(f_i, e_j)] / f_count[f_i]), loss)
+            dice_ef[(f_i, e_j)] = fe_count[(f_i, e_j)] / f_count[f_i]
+            if it % 500000 == 0:
+                sys.stderr.write(".")
+        sys.stderr.write("; loss is {0:2.5f}.\n".format(loss))
+        if k == 5: return
+
+
+def train_fe():
+    '''train f|e, dice is t_k'''
+    def init():
+        '''uniformly initilize fe_count'''
+        f_word_len = len(set([w for s in open(f_data) for w in s.strip().split()]))
+        return 1.0/f_word_len
+
+    dice_default = init()
+    for k in range(5):
+        sys.stderr.write("train_fe: Iteration {0} ".format(k))
         e_count = {}
         fe_count = {}
 
@@ -45,16 +78,16 @@ def train():
             for f_i in f:
                 Z = 0.0
                 for e_j in e:
-                    if (f_i, e_j) not in dice: dice[(f_i, e_j)]=dice_default
-                    Z += dice[(f_i, e_j)]
+                    if (f_i, e_j) not in dice_fe: dice_fe[(f_i, e_j)] = dice_default
+                    Z += dice_fe[(f_i, e_j)]
                 for e_j in e:
-                    c = dice[(f_i, e_j)]/Z
+                    c = dice_fe[(f_i, e_j)]/Z
                     fe_count[(f_i, e_j)] = fe_count.get((f_i, e_j), 0.0)+c
                     e_count[e_j] = e_count.get(e_j, 0.0)+c
         loss = 0.0
         for (it, (f_i, e_j)) in enumerate(fe_count.keys()):
-            loss = max(abs(dice[(f_i, e_j)]-fe_count[(f_i, e_j)] / e_count[e_j]), loss)
-            dice[(f_i, e_j)] = fe_count[(f_i, e_j)] / e_count[e_j]
+            loss = max(abs(dice_fe[(f_i, e_j)]-fe_count[(f_i, e_j)] / e_count[e_j]), loss)
+            dice_fe[(f_i, e_j)] = fe_count[(f_i, e_j)] / e_count[e_j]
             if it % 500000 == 0:
                 sys.stderr.write(".")
         sys.stderr.write("; loss is {0:2.5f}.\n".format(loss))
@@ -63,6 +96,10 @@ def train():
 
 
 def decode():
+    '''decode best alignment based on EM'''
+    for (f, e) in dice_ef:
+        if (f, e) in dice_fe:
+            dice[(f, e)] = dice_fe[(f, e)]
     for (f, e) in bitext:
         for i, f_i in enumerate(f):
             bestp, bestj = 0.0, 0.0
@@ -72,6 +109,12 @@ def decode():
                     bestj = j
             sys.stdout.write("%i-%i " % (i, bestj))
         sys.stdout.write("\n")
+
+def train():
+    '''train body'''
+    sys.stderr.write("Training with expected maximization algorithm...\n")
+    train_ef()
+    train_fe()
 
 def main():
     '''main'''
--- HW3-aligner/answer/readme.yingxiuc.md
+++ HW3-aligner/answer/readme.yingxiuc.md
@@ -1,6 +1,10 @@
+# Personal git track
 
-Your documentation
-------------------
+1.d72b97b10665f830e8329ffaa72ec32926642f53
+    1.Implemented a quicker baseline. 
+    2.Modify code style based on PEP8.
+    3.Instead of comparing len(feats) times, compare len(sentence) per sentence.
+    4.Read paper about multiple representation and we decided we'd better to avoid this.
 
-Make it so.
-
+2.d7774552f4be991755f75140dec0f4b774294ed0
+    1.change codes to do key checking instead of using defaultdict __missingkey__, it will take much less time.
