9448348de3fc81e0085a2bf6b09d7c9c5e160dff
liufanl <liufanl@sfu.ca>
2017-11-11 20:11:12 -0800

    update by using matrix and list inside of dict

--- HW3-aligner/llr_align.py
+++ HW3-aligner/llr_align.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import optparse, sys, os, logging
 from collections import defaultdict
 import copy, math
@@ -24,128 +25,126 @@ bitext = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f
 e_word_count = defaultdict(int)
 f_word_count = defaultdict(int)
 fe_word_count = defaultdict(int)
-# e_group_count = defaultdict(int)
-# f_group_count = defaultdict(int)
 for (n, (f, e)) in enumerate(bitext):
   for f_i in set(f):
     f_word_count[f_i] += 1
     for e_j in set(e):
       fe_word_count[(f_i,e_j)] += 1
-      # e_group_count[e_j] += 1
-      # f_group_count[f_i] += 1
   for e_j in set(e):
     e_word_count[e_j] += 1
 
-
-# sum_e = sum(e_group_count.values())
-# sum_f = sum(f_group_count.values())
-sum_e = sum(e_word_count.values())
-sum_f = sum(f_word_count.values())
-sum_fe = sum(fe_word_count.values())
-# print sum_e, sum_f, sum_fe
-# for word in f_group_count:
-#     print word
-#     print f_group_count[word]
-
-N = opts.num_sents
-# t_k = defaultdict(float)
-V_f = len(f_word_count)
-t_k = dict.fromkeys(fe_word_count.keys(), 1.0/V_f)
-
-def probXY(f_word, e_word):
-    count_fe = 0
-    for (k, (f_i, e_j)) in enumerate(fe_word_count.keys()):
-        if f_i == f_word and e_j == e_word:
-            count_fe += fe_word_count[(f_i, e_j)]
-    return 1.0 * count_fe / sum_fe
-
-def probbarXY(f_word, e_word):
-    count_fbar_e, count_f_ebar, count_fbar_ebar = 0, 0, 0
-    for (k, (f_i, e_j)) in enumerate(fe_word_count.keys()):
-        if f_i != f_word and e_j == e_word:
-            count_fbar_e += fe_word_count[(f_i, e_j)]
-        elif f_i == f_word and e_j != e_word:
-            count_f_ebar += fe_word_count[(f_i, e_j)]
-        elif f_i != f_word and e_j != e_word:
-            count_fbar_ebar += fe_word_count[(f_i, e_j)]
-    return 1.0 * count_fbar_e / sum_fe, 1.0 * count_f_ebar / sum_fe, 1.0 * count_fbar_ebar / sum_fe
-
-print probbarXY("transports", ",")
-# def probXYbar(f_word, e_word):
-#     count_fe = 0
-#     for (k, (f_i, e_j)) in enumerate(fe_word_count.keys()):
-#         if f_i == f_word and e_j != e_word:
-#             count_fe += fe_word_count[(f_i, e_j)]
-#     return 1.0 * count_fe / sum_fe
+# print fe_word_count[("MATIÈRES", "contents")]
+# print fe_word_count[("CRÉDITS", "supply")]
+# print fe_word_count[("prière", "prayers")]
+# print fe_word_count[("prière", "prayers")]
 
 
-# def probXbarYbar(f_word, e_word):
-#     count_fe = 0
-#     for (k, (f_i, e_j)) in enumerate(fe_word_count.keys()):
-#         if f_i != f_word and e_j != e_word:
-#             count_fe += fe_word_count[(f_i, e_j)]
-#     return 1.0 * count_fe / sum_fe
+# f words and e words list
+f_list = f_word_count.keys()
+e_list = e_word_count.keys()
 
+# total occurrence number of single f word and single e word
+total_f = sum(f_word_count.values())
+total_e = sum(e_word_count.values())
+total_fe = sum(fe_word_count.values())
 
+# number of f words and e words
+num_f = len(f_list)
+num_e = len(e_list)
+
+# a matrix to repesent the combinations of f word and e word
+fe_matrix = [[0 for x in range(num_e)] for y in range(num_f)]
+
+# total occurrence number of (f, e) respect to a specific f word and all other e words
+sum_f = [0] * num_f
+# total occurrence number of (f, e) respect to a specific e word and all other f words
+sum_e = [0] * num_e
+
+for i in range(num_f):
+  for j in range(num_e):
+    fe_matrix[i][j] = fe_word_count[(f_list[i], e_list[j])]
+    sum_f[i] += fe_matrix[i][j]
+
+for j in range(num_e):
+  for i in range(num_f):
+    sum_e[j] += fe_matrix[i][j]
+
+t_k = defaultdict(float)
 sum_e_LLR_score = defaultdict(float)
+N = opts.num_sents
+
 for (k, (f_i, e_j)) in enumerate(fe_word_count.keys()):
-    # p_f = 1.0 * f_group_count[f_i] / sum_f
-    # p_e = 1.0 * e_group_count[e_j] / sum_e
-    p_f = 1.0 * f_word_count[f_i] / sum_f
-    p_e = 1.0 * e_word_count[e_j] / sum_e
-    p_fe = 1.0 * fe_word_count[(f_i, e_j)] / sum_fe
-    # print f_i, p_f, p_e, p_fe
-    # print f_i, f_group_count[f_i], sum_f
+  f_index = f_list.index(f_i)
+  e_index = e_list.index((e_j))
 
+  C_f_e = fe_matrix[f_index][e_index]
 
-    if p_fe > p_f * p_e:
-        # p_f_e_bar = 1.0 * (f_group_count[f_i] - fe_word_count[(f_i, e_j)]) / sum_fe
-        # p_f_bar_e = 1.0 * (e_group_count[e_j] - fe_word_count[(f_i, e_j)]) / sum_fe
-        # p_f_bar_e_bar = 1.0 * (sum_fe - fe_word_count[(f_i, e_j)]) / sum_fe
-        # p_f_e_bar = probXYbar(f_i, e_j)
-        # p_f_bar_e = probXbarY(f_i, e_j)
-        # p_f_bar_e_bar = probXbarYbar(f_i, e_j)
-        p_f_e_bar, p_f_bar_e, p_f_bar_e_bar = probbarXY(f_i, e_j)
+  p_f = 1.0 * f_word_count[f_i] / total_f
+  p_e = 1.0 * e_word_count[e_j] / total_e
+  P_fe = 1.0 * C_f_e / total_fe
 
-        f_e = p_fe * math.log10(p_fe / (p_f * p_e))
-        f_e_bar = p_f_e_bar * math.log10(p_f_e_bar / (p_f * (1 - p_e)))
-        if p_f_bar_e / ((1 - p_f) * p_e) <=0:
-            print p_f_bar_e / ((1 - p_f) * p_e)
+  if P_fe > p_f * p_e:
+    C_f_Note = sum_f[f_index] - C_f_e
+    C_Notf_e = sum_e[e_index] - C_f_e
+    C_Notf_Note = total_fe - C_f_Note - C_Notf_e + C_f_e
 
-        f_bar_e = p_f_bar_e * math.log10(p_f_bar_e / ((1 - p_f) * p_e))
-        f_bar_e_bar = p_f_bar_e_bar * math.log10(p_f_bar_e_bar / ((1 - p_f) * (1 - p_e)))
-        # print f_e_bar
-        # print "\n"
+    if C_f_Note == 0 or C_Notf_e == 0: 
+    # i.e. sum_f[f_index] == C_f_e or sum_e[e_index] == C_f_e
+    # this indicates that f_i is only related to e_j in the original dataset
+      t_k[(f_i, e_j)] = 1
+    else:
+      P_f_Note = 1.0 * C_f_Note / total_fe
+      P_Notf_e = 1.0 * C_Notf_e / total_fe
+      P_Notf_Note = 1.0 * C_Notf_Note / total_fe
 
+      G_f_e = P_fe * math.log10(P_fe / (p_f * p_e))
 
-        t_k[(f_i, e_j)] = 2 * N * (f_e + f_e_bar + f_bar_e + f_bar_e_bar)
-        sum_e_LLR_score[e_j] += t_k[(f_i, e_j)]
-        # print t_k[(f_i, e_j)]
+      Div_f_Note = P_f_Note / (p_f * (1 - p_e))
+      if Div_f_Note > 0:
+        G_f_Note = P_f_Note * math.log10(Div_f_Note)
+
+      Div_Notf_e = P_Notf_e / ((1 - p_f) * p_e)
+      if Div_Notf_e > 0:
+        G_Notf_e = P_Notf_e * math.log10(Div_Notf_e)
+
+      Div_Notf_Note = P_Notf_Note / ((1 - p_f) * (1 - p_e))
+      if Div_Notf_Note > 0:
+        G_Notf_Note = P_Notf_Note * math.log10(Div_Notf_Note)
+
+      '''
+      if P_f_Note / (p_f * (1 - p_e)) <=0:
+        print "P_f_Note:"
+        print f_index, e_index, C_f_e, fe_word_count[(f_i, e_j)]
+        print C_f_Note, sum_f[f_index], C_f_e
+        print P_f_Note
+        print f_i, e_j, P_f_Note / (p_f * (1 - p_e))
+        print "\n"
+
+      if P_Notf_e / ((1 - p_f) * p_e) <=0:
+        print "P_Notf_e:"
+        print f_index, e_index, C_f_e, fe_word_count[(f_i, e_j)]
+        print C_Notf_e, sum_f[f_index], C_f_e
+        print P_Notf_e
+        print f_i, e_j, P_Notf_e / ((1 - p_f) * p_e)
+
+      if P_Notf_Note / ((1 - p_f) * (1 - p_e)) <=0:
+        print "P_Notf_Note:"
+        print f_index, e_index, C_f_e, fe_word_count[(f_i, e_j)]
+        print C_Notf_Note, sum_f[f_index], C_f_e
+        print P_Notf_Note
+        print f_i, e_j, P_Notf_Note / ((1 - p_f) * (1 - p_e))
+      '''
+
+      t_k[(f_i, e_j)] = 2 * N * (G_f_e + G_f_Note + G_Notf_e + G_Notf_Note)
+  else:
+    t_k[(f_i, e_j)] = 1.0 / num_f
+
+  sum_e_LLR_score[e_j] += t_k[(f_i, e_j)]
 
 max_sum_e_LLR_score = max(sum_e_LLR_score.values())
 
-# e_sum = defaultdict(float)
 for f,e in t_k:
-    t_k[(f,e)] = t_k[(f,e)] / max_sum_e_LLR_score
-    # print t_k[(f,e)]
-    # e_sum[e] += t_k[(f,e)]
-
-# print t_k[("transports", ",")]
-# print max(t_k.values())
-# print min(t_k.values())
-# print max(e_sum.values())
-# print min(e_sum.values())
-
-
-
-# V_f = len(f_word_count)
-# t_k = dict.fromkeys(fe_word_count.keys(), 1.0/V_f)
-
-V = V_f
-n = 1
-# Precision = 0.294292
-# Recall = 0.399208
-# AER = 0.669803
+  t_k[(f,e)] = t_k[(f,e)] / max_sum_e_LLR_score
 
 iteration = 5
 
@@ -165,7 +164,6 @@ for i in xrange(iteration):
 
   for (k, (f_i, e_j)) in enumerate(fe_count.keys()):
     t_k[(f_i, e_j)] = 1.0 * fe_count[(f_i,e_j)] / e_count[e_j]
-    # t_k[(f_i, e_j)] = (1.0 * fe_count[(f_i,e_j)] + n) / (e_count[e_j] + n * V)
     if k % 50000 == 0:
       sys.stderr.write(".")
   sys.stderr.write("\n")
@@ -186,8 +184,6 @@ for (n, (f, e)) in enumerate(bitext):
 del e_word_count
 del f_word_count
 del fe_word_count
-# del e_group_count
-# del f_group_count
 del t_k
 del sum_e_LLR_score
 del fe_count
