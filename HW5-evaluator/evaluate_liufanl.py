104bb0e4e83e9552c9f80086ae40372c1e0af915
liufanl <liufanl@sfu.ca>
2017-12-05 15:50:38 -0800

    add chunk penalty

--- HW5-evaluator/evaluate_liufanl.py
+++ HW5-evaluator/evaluate_liufanl.py
@@ -4,13 +4,16 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import argparse # optparse is deprecated
 from itertools import islice # slicing for iterators
-from math import pow
+from math import pow, exp
 import copy
 # import nltk
 # nltk.download('wordnet')
 from nltk.stem.porter import *
 from nltk.corpus import wordnet
 
+ALPHA = 0.73
+BETA = 1.0
+GAMMA = 0.21
 
 def sentence_preprocess(word_list):
     lowercase_word_list = [word.lower() for word in word_list]
@@ -74,20 +77,51 @@ def word_synonym_matches(h, h_stemmed_matched, ref_not_stemmed_match):
 # def word_matches(h):
 #     return sum(1 for w in h if w in ref)
 
-def meteor_calculate(h_total_matched, ref):
+def get_chunk(hypo, ref):
+    '''TODO: need further test
+    ref : the cat sat on the mat
+    hypo: the cat was sat on the mat
+    chunk = 2
+    '''
+    ref_sentence = ' '.join(ref)
+    chunks = i = j = 0
+    while i < len(hypo):
+        if hypo[i] in ref:
+            chunks += 1
+            j = i
+            hypo_shard = hypo[i]
+            while hypo_shard in ref_sentence and j < len(hypo):
+                j += 1
+                hypo_shard = ' '.join(hypo[i:j+1])
+        if j > i: i = j
+        else: i += 1
+    return chunks
+
+def meteor_calculate(h, h_total_matched, ref):
     # rset = set(ref)
     # h_match = word_matches(h, rset)
     h_match = h_total_matched.count(True) + 0.1 # plus 0.1 to avoide zero match
     h_uni_precision = 1.0 * h_match / len(h_total_matched)
     h_uni_recall = 1.0 * h_match / len(ref)
-    harmonic_mean = 1.0 * 10 * h_uni_precision * h_uni_recall / (h_uni_recall + 9 * h_uni_precision)
+    harmonic_mean = 1.0 * h_uni_precision * h_uni_recall / ( (1- ALPHA) * h_uni_recall + ALPHA * h_uni_precision)
 
-    # h_chunk = 
-    # penalty = 0.5 * math.pow(1.0 * h_chunk / h_match, 3)
-    # meteor = harmonic_mean * (1 - penalty)
+    h_chunk = get_chunk(h, ref)
+    chunk_penalty = GAMMA * pow(1.0 * h_chunk / h_match, BETA)
+    meteor = harmonic_mean * (1 - chunk_penalty)
 
-    meteor = harmonic_mean
     return meteor
+
+def length_penalty(h, ref):
+    h_length = len(h)
+    ref_length = len(ref)
+    h_length_penalty = 0
+    if h_length < ref_length:
+        h_length_penalty = exp(1 - 1.0 * ref_length / h_length)
+    elif h_length == ref_length:
+        h_length_penalty = 1
+    else: # h_length > ref_length
+        h_length_penalty = exp(1 - 1.0 * h_length / ref_length)
+    return h_length_penalty
 
 
 def main():
@@ -119,6 +153,7 @@ def main():
         h1_matched = [False] * len(h1_clean)
         h2_matched = [False] * len(h2_clean)
 
+        # METEOR
         h1_exact_matched, ref_not_exact_match_h1 = word_exact_matches(h1_clean, ref_clean_h1, h1_matched)
         h2_exact_matched, ref_not_exact_match_h2 = word_exact_matches(h2_clean, ref_clean_h2, h2_matched)
 
@@ -129,11 +164,21 @@ def main():
         h2_synonym_matched = word_synonym_matches(h2_clean, h2_stemmed_matched, ref_not_stemmed_match_h2)
 
 
-        h1_word_match_score = meteor_calculate(h1_synonym_matched, ref_clean)
-        h2_word_match_score = meteor_calculate(h2_synonym_matched, ref_clean)
+        h1_meteor_score = meteor_calculate(h1_clean, h1_synonym_matched, ref_clean)
+        h2_meteor_score = meteor_calculate(h2_clean, h2_synonym_matched, ref_clean)
 
-        print(1 if h1_word_match_score > h2_word_match_score else # \begin{cases}
-                (0 if h1_word_match_score == h2_word_match_score
+        # LEPOR
+        # h1_length_penalty = length_penalty(h1_clean, ref_clean)
+        # h2_length_penalty = length_penalty(h2_clean, ref_clean)
+
+        h1_length_penalty = 1
+        h2_length_penalty = 1
+
+        h1_score = h1_meteor_score * h1_length_penalty
+        h2_score = h2_meteor_score * h2_length_penalty
+
+        print(1 if h1_score > h2_score else # \begin{cases}
+                (0 if h1_score == h2_score
                     else -1)) # \end{cases}
 
 # convention to allow import of this file as a module
